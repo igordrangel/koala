51814d3f77b4b93273e5bf01a97d164e84616d5a
Igor Rangel <igor.rangel_meta@prestador.globo>
2026-05-11 11:27:34 -0300

    feat(dropdown): refatorar componente dropdown para melhorar acessibilidade e estrutura feat(modal): substituir elemento dialog por div para compatibilidade com navegadores feat(list): adicionar suporte a filtros dinâmicos e recarregamento de dados

--- libs/ui/src/app/shared/base/list.base.ts
+++ libs/ui/src/app/shared/base/list.base.ts
@@ -1,4 +1,4 @@
-import { computed, Directive, ResourceRef, signal } from '@angular/core';
+import { computed, Directive, effect, input, ResourceRef, signal } from '@angular/core';
 import { FilterDefinition, FilterValue } from '../components/filter/filter.models';
 import { OrderBy } from '../components/table/ordered-header-col';
 
@@ -8,7 +8,7 @@ export interface DatalistResponse<TDataItem> {
 }
 
 @Directive()
-export abstract class ListBase<TDataItem = any> {
+export abstract class ListBase<TDataItem = any, TQuery = any> {
   protected readonly currentPage = signal<number | null>(1);
   protected readonly pageSize = signal<number | null>(10);
   protected readonly totalItems = signal<number | null>(0);
@@ -22,5 +22,33 @@ export abstract class ListBase<TDataItem = any> {
 
   protected abstract readonly datalist: ResourceRef<DatalistResponse<TDataItem>>;
 
-  constructor(protected readonly filterDefinitions: FilterDefinition[] = []) {}
+  readonly reload = input<boolean>(false);
+
+  protected get filterPayload(): TQuery {
+    const payload: Partial<TQuery> = {};
+
+    this.filter()?.forEach((item) => {
+      payload[item.key as keyof TQuery] = item.value as any;
+    });
+
+    return payload as TQuery;
+  }
+
+  protected get filterParams() {
+    return {
+      filter: this.filter(),
+      page: this.currentPage(),
+      pageSize: this.pageSize(),
+      sortBy: this.orderedBy()?.field,
+      order: this.orderedBy()?.direction,
+    };
+  }
+
+  constructor(protected readonly filterDefinitions: FilterDefinition[] = []) {
+    effect(() => {
+      if (this.reload()) {
+        this.datalist.reload();
+      }
+    });
+  }
 }
