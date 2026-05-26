6083790d46ce1cf0edb9e96d25998b1c86640338
Igor Rangel <igor.rangel_meta@prestador.globo>
2026-05-26 16:07:57 -0300

    feat: adicionar instalação de utilitário no setup de projeto existente

--- libs/cli/commands/init.ts
+++ libs/cli/commands/init.ts
@@ -1,5 +1,4 @@
 import { logHeader } from '../utils/cli-ui';
-import { installUtil } from '../utils/install-util';
 import { setupExistingProject } from '../utils/setup-existing-project';
 
 export interface InitArgs {
@@ -15,6 +14,4 @@ export async function runInitCommand(args: InitArgs): Promise<void> {
   logHeader(logger, 'KOALA PROJECT INITIALIZER', `Project: ${projectName}`);
 
   await setupExistingProject(projectName, verbose);
-
-  installUtil(projectName, 'control-changes');
 }
