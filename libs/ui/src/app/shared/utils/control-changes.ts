6a428dd28ffd4bc158a1701531d35784b3ed6fcd
Igor Rangel <igor.rangel_meta@prestador.globo>
2026-05-29 19:50:39 -0300

    refactor: simplify controlChanges utility function
    - Updated the controlChanges function to accept AbstractControl instead of FormControl, enhancing flexibility.
    - Removed unnecessary filtering logic, streamlining the function to directly return valueChanges as a Signal.

--- libs/ui/src/app/shared/utils/control-changes.ts
+++ libs/ui/src/app/shared/utils/control-changes.ts
@@ -1,9 +1,7 @@
+import { Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { FormControl } from '@angular/forms';
-import { filter } from 'rxjs/internal/operators/filter';
+import { AbstractControl } from '@angular/forms';
 
-export function controlChanges<T extends FormControl>(control: T) {
-  return toSignal<T['value']>(control.valueChanges.pipe(filter(() => control.valid)), {
-    initialValue: control.value,
-  });
+export function controlChanges<T>(control: AbstractControl<T>): Signal<T> {
+  return toSignal(control.valueChanges, { initialValue: control.value });
 }
